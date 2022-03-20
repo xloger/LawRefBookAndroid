b970f9007da512d5757ffcdccab1ad8f93586ce5
xloger <phoenix@xloger.com>
2022-03-20 14:39:29 +0800

    初始化项目

---
+++ app/src/main/java/com/xloger/lawrefbook/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.xloger.lawrefbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xloger.lawrefbook.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
