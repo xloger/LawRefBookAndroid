1d8e4c0090874b33e2af1aff9cdb563afc7073fc
xloger <phoenix@xloger.com>
2022-04-08 00:10:34 +0800

    更新关于页面。

--- app/src/main/java/com/xloger/lawrefbook/ui/about/AboutFragment.kt
+++ app/src/main/java/com/xloger/lawrefbook/ui/about/AboutFragment.kt
@@ -1,12 +1,20 @@
 package com.xloger.lawrefbook.ui.about
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.xloger.lawrefbook.BuildConfig
 import com.xloger.lawrefbook.databinding.AboutFragmentBinding
+import com.xloger.lawrefbook.util.customStyle
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AboutFragment : Fragment() {
@@ -26,7 +34,77 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
         initToolBar()
+    }
+
+    @SuppressLint("ResourceType")
+    private fun initView() {
+        binding.aboutLawContent.apply {
+            val spannable = SpannableString(text)
+            spannable.customStyle(spannable, text.toString(), 0, "国家法律法规数据库（https://flk.npc.gov.cn）") {
+                return@customStyle object : ClickableSpan() {
+                    override fun onClick(p0: View) {
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://flk.npc.gov.cn")).let {
+                            startActivity(it)
+                        }
+                    }
+
+                }
+            }
+            text = spannable
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+        binding.aboutAppContent.apply {
+            val spannable = SpannableString(text)
+            spannable.customStyle(spannable, text.toString(), 0, "https://github.com/LawRefBook/Laws") {
+                return@customStyle object : ClickableSpan() {
+                    override fun onClick(p0: View) {
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/LawRefBook/Laws")).let {
+                            startActivity(it)
+                        }
+                    }
+
+                }
+            }
+            text = spannable
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+        binding.aboutVersion.apply {
+            text = "当前版本：V${BuildConfig.VERSION_NAME}"
+        }
+        binding.aboutCheckUpdate.apply {
+            setOnClickListener {
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/xloger/LawRefBookAndroid/releases")).let {
+                    startActivity(it)
+                }
+            }
+        }
+        binding.aboutUpdateContent.apply {
+            val spannable = SpannableString(text)
+            spannable.customStyle(spannable, text.toString(), 0, "Github") {
+                return@customStyle object : ClickableSpan() {
+                    override fun onClick(p0: View) {
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/xloger/LawRefBookAndroid")).let {
+                            startActivity(it)
+                        }
+                    }
+
+                }
+            }
+            spannable.customStyle(spannable, text.toString(), 0, "Google Play") {
+                return@customStyle object : ClickableSpan() {
+                    override fun onClick(p0: View) {
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.xloger.lawrefbook")).let {
+                            startActivity(it)
+                        }
+                    }
+
+                }
+            }
+            text = spannable
+            movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 
     private fun initToolBar() {
--- app/src/main/java/com/xloger/lawrefbook/util/KotlinExpand.kt
+++ app/src/main/java/com/xloger/lawrefbook/util/KotlinExpand.kt
@@ -2,6 +2,7 @@ package com.xloger.lawrefbook.util
 
 import android.content.Context
 import android.content.res.Resources
+import android.text.Spannable
 import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
@@ -51,3 +52,16 @@ fun View.visibleOrInvisible(isVisible: Boolean) {
 fun Context.themeColor(@AttrRes attrRes: Int): Int = TypedValue()
     .apply { theme.resolveAttribute (attrRes, this, true) }
     .data
+
+
+fun Spannable.customStyle(spannable: Spannable, content: String, start: Int, styleText: String, styleBlock: () -> Any) {
+    if (content.isBlank()) return
+    val startPos: Int = start + content.substring(start).indexOf(styleText)
+    if (startPos > -1) {
+        val endPos: Int = startPos + styleText.length
+        if (content.substring(endPos).contains(styleText)) {
+            customStyle(this, content, endPos, styleText, styleBlock)
+        }
+        spannable.setSpan(styleBlock(), startPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+}
